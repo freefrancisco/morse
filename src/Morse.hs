48456703c50a828a013e958d6c1f03d7935eac89
Francisco Gutierrez <freefrancisco@gmail.com>
2017-07-13 10:26:41 -0700

    initial commit after entering all morse stuff by hand, don't wanna lose this

---
+++ src/Morse.hs
@@ -0,0 +1,56 @@
+module Morse
+  ( Morse
+  , charToMorse
+  , morseToChar
+  , stringToMorse
+  , letterToMorse
+  , morseToLetter
+  ) where
+
+import qualified Data.Map as M
+
+type Morse = String
+
+charToMorse = undefined
+morseToChar = undefined
+stringToMorse = undefined
+morseToLetter = undefined
+
+letterToMorse :: (M.Map Char Morse)
+letterToMorse = M.fromList [
+      ('a', ".-")
+    , ('b', "-...")
+    , ('c', "-.-.")
+    , ('d', "-..")
+    , ('e', ".")
+    , ('f', "..-.")
+    , ('g', "--.")
+    , ('h', "....")
+    , ('i', "..")
+    , ('j', ".---")
+    , ('k', "-.-")
+    , ('l', ".-..")
+    , ('m', "--")
+    , ('o', "---")
+    , ('p', ".--.")
+    , ('q', "--.-")
+    , ('r', ".-.")
+    , ('s', "...")
+    , ('t', "-")
+    , ('u', "..-")
+    , ('v', "...-")
+    , ('w', ".--")
+    , ('x', "-..-")
+    , ('y', "-.--")
+    , ('z', "--..")
+    , ('1', ".----")
+    , ('2', "..---")
+    , ('3', "...--")
+    , ('4', "....-")
+    , ('5', ".....")
+    , ('6', "-....")
+    , ('7', "--...")
+    , ('8', "---..")
+    , ('9', "----.")
+    , ('0', "-----")
+    ]
